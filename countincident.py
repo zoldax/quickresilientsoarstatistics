7062953f6df4f6b6b65b70aa0f5a08b3692303ae
Pascal Weber <zoldax@gmail.com>
2024-07-09 15:41:23 +0200

    Change query_paged?field_handle=-1

--- countincident.py
+++ countincident.py
@@ -64,7 +64,8 @@ def fetch_incidents(res_client):
                 }
             ]
         }
-        response = res_client.post("/incidents/query_paged?return_level=full", payload=payload)
+        #response = res_client.post("/incidents/query_paged?return_level=full", payload=payload)
+        response = res_client.post("/incidents/query_paged?ield_handle=-1", payload=payload)
         return response.get("data", [])
     except Exception as e:
         logging.error("Error fetching incidents: %s", e)
